a248a870baa50a3545c07998920fd5c9f3dc84f9
SlientPickaxe <Noobsapiens12@outlook.com>
2026-05-06 18:46:58 +0200

    Refactor Packages module for better dependency handling

--- sys/modules/opus/packages.lua
+++ sys/modules/opus/packages.lua
@@ -1,96 +1,313 @@
 local Util = require('opus.util')
 
+
+
 local fs        = _G.fs
+
 local textutils = _G.textutils
+
+
 
 local PACKAGE_DIR = 'packages'
 
+
+
 local Packages = { }
 
+
+
 function Packages:installed()
+
 	local list = { }
+
+
 
 	if fs.exists(PACKAGE_DIR) then
+
 		for _, dir in pairs(fs.list(PACKAGE_DIR)) do
+
 			local path = fs.combine(fs.combine(PACKAGE_DIR, dir), '.package')
-			list[dir] = Util.readTable(path)
+
+			local data = Util.readTable(path)
+
+
+
+			if not data then
+
+				print('Chyba v souboru: ' .. path)
+
+			else
+
+				list[dir] = data
+
+			end
+
 		end
+
 	end
 
+
+
 	return list
+
 end
+
+
 
 function Packages:installedSorted()
-	local list = { }
 
-	for k, v in pairs(self.installed()) do
-		v.name = k
-		v.deps = { }
-		table.insert(list, v)
-		for _, v2 in pairs(v.required or { }) do
-			v.deps[v2] = true
+	local installed = self:installed()
+
+	local result = { }
+
+	local visited = { }
+
+	local visiting = { }
+
+
+
+	local function visit(name)
+
+		if visiting[name] then
+
+			print('Cyklická závislost: ' .. name)
+
+			return
+
 		end
+
+
+
+		if not visited[name] then
+
+			visiting[name] = true
+
+
+
+			local pkg = installed[name]
+
+			if pkg then
+
+				pkg.name = name
+
+				pkg.deps = pkg.deps or { }
+
+
+
+				for _, dep in pairs(pkg.required or { }) do
+
+					visit(dep)
+
+				end
+
+
+
+				table.insert(result, pkg)
+
+			else
+
+				print('Chybí balíček: ' .. tostring(name))
+
+			end
+
+
+
+			visiting[name] = nil
+
+			visited[name] = true
+
+		end
+
 	end
 
-	table.sort(list, function(a, b)
-		return not not (b.deps and b.deps[a.name])
-	end)
 
-	table.sort(list, function(a, b)
-		return not (a.deps and a.deps[b.name])
-	end)
 
-	return list
+	for name in pairs(installed) do
+
+		visit(name)
+
+	end
+
+
+
+	return result
+
 end
+
+
 
 function Packages:list()
+
 	if not fs.exists('usr/config/packages') then
+
 		self:downloadList()
+
 	end
-	return Util.readTable('usr/config/packages') or { }
+
+
+
+	local t = Util.readTable('usr/config/packages')
+
+
+
+	if not t then
+
+		print('Chyba při čtení usr/config/packages')
+
+		return { }
+
+	end
+
+
+
+	return t
+
 end
+
+
 
 function Packages:isInstalled(package)
+
 	return self:installed()[package]
+
 end
+
+
 
 function Packages:downloadList()
+
 	local packages = {
+
 		[ 'develop-1.8' ] = 'https://raw.githubusercontent.com/kepler155c/opus-apps/develop-1.8/packages.list',
+
 		[ 'master-1.8' ] = 'https://raw.githubusercontent.com/kepler155c/opus-apps/master-1.8/packages.list',
+
 	}
 
+
+
 	if packages[_G.OPUS_BRANCH] then
+
 		Util.download(packages[_G.OPUS_BRANCH], 'usr/config/packages')
+
+	else
+
+		print('Neznámá větev: ' .. tostring(_G.OPUS_BRANCH))
+
 	end
+
 end
+
+
 
 function Packages:downloadManifest(package)
+
 	local list = self:list()
+
 	local url = list and list[package]
 
-	if url then
-		local c = Util.httpGet(url)
-		if c then
-			c = textutils.unserialize(c)
-			if c then
-				c.repository = c.repository:gsub('{{OPUS_BRANCH}}', _G.OPUS_BRANCH)
-				return c
-			end
-		end
+
+
+	if not url then
+
+		print('Nenalezen URL pro balíček: ' .. tostring(package))
+
+		return
+
 	end
+
+
+
+	local c = Util.httpGet(url)
+
+
+
+	if not c then
+
+		print('HTTP chyba pro: ' .. url)
+
+		return
+
+	end
+
+
+
+	local ok = textutils.unserialize(c)
+
+
+
+	if not ok then
+
+		print('Chyba unserialize pro: ' .. url)
+
+		return
+
+	end
+
+
+
+	if not ok.repository then
+
+		print('Chybí repository v manifestu: ' .. tostring(package))
+
+		return ok
+
+	end
+
+
+
+	ok.repository = ok.repository:gsub('{{OPUS_BRANCH}}', _G.OPUS_BRANCH)
+
+	return ok
+
 end
 
+
+
 function Packages:getManifest(package)
+
 	local fname = 'packages/' .. package .. '/.package'
+
+
+
 	if fs.exists(fname) then
+
 		local c = Util.readTable(fname)
-		if c and c.repository then
-			c.repository = c.repository:gsub('{{OPUS_BRANCH}}', _G.OPUS_BRANCH)
-			return c
+
+
+
+		if not c then
+
+			print('Chyba v lokálním manifestu: ' .. fname)
+
+			return
+
 		end
+
+
+
+		if c.repository then
+
+			c.repository = c.repository:gsub('{{OPUS_BRANCH}}', _G.OPUS_BRANCH)
+
+		else
+
+			print('Chybí repository v lokálním manifestu: ' .. fname)
+
+		end
+
+
+
+		return c
+
 	end
+
+
+
 	return self:downloadManifest(package)
+
 end
+
+
 
 return Packages
